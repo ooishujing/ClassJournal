cf81a952f095e654ccf91a9af30fc8577a021e95
19047433 <19047433@myrp.edu.sg>
2021-04-29 13:16:19 +0800

    Nicole Grade.java

--- app/src/main/java/sg/edu/rp/c346/classjournal/Grade.java
+++ app/src/main/java/sg/edu/rp/c346/classjournal/Grade.java
@@ -1,4 +1,21 @@
 package sg.edu.rp.c346.classjournal;
 
-public class Grade {
+import java.io.Serializable;
+
+public class Grade implements Serializable {
+    private int week;
+    private String grade;
+
+    public Grade(int week, String grade) {
+        this.week = week;
+        this.grade = grade;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
 }
