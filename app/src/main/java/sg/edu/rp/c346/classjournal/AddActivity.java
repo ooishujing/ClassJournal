0777f2911cf281a29f9e6eed3ca5da05c47ab23e
ooishujing <18016012@myrp.edu.sg>
2021-04-29 11:55:27 +0800

    updated

---
+++ app/src/main/java/sg/edu/rp/c346/classjournal/AddActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.classjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
