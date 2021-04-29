6c521e2ed7ae5bd2042070b8ea26ee33ed3a65ff
daryl13579 <19042545@myrp.edu.sg>
2021-04-29 13:05:02 +0800

    view

--- app/src/main/java/sg/edu/rp/c346/classjournal/InfoActivity.java
+++ app/src/main/java/sg/edu/rp/c346/classjournal/InfoActivity.java
@@ -2,13 +2,56 @@ package sg.edu.rp.c346.classjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
+
+    Button btnInfo,btnAdd,btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnInfo = findViewById(R.id.btnInfo);
+        btnEmail = findViewById(R.id.btnEmail);
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.rp.edu.sg/"));
+                startActivity(intent);
+            }
+        });
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_TEXT,"Hi faci, \n\n" +
+                        "I am ... \n" +
+                        "Please see my remarks so far,thank you! \n" +
+                        "");
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,"Choose an Email client :"));
+
+            }
+        });
     }
 }
